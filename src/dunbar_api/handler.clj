2b96971fc95891b956c2066ec0f69b32e23bd91b
John Cowie <john@imin.co>
2016-11-04 08:14:39 +0000

    Posted friend name is saved to the database

--- src/dunbar_api/handler.clj
+++ src/dunbar_api/handler.clj
@@ -5,22 +5,40 @@
             [scenic.routes :refer [scenic-handler]]
             [dunbar-api.routes :as r]
             [dunbar-api.db :as db]
-            [ring.util.response :refer [response content-type status]])
+            [ring.util.response :refer [response content-type status]]
+            [clojure.string :as str])
   (:gen-class))
 
+
+(defn add-user [friend]
+  (assoc friend :user "john"))
+
+(def trimmed-lowercase (comp str/trim str/lower-case))
+
+(defn gen-id [{:keys [firstName lastName] :as friend}]
+  (let [id (str (trimmed-lowercase firstName) "-" (trimmed-lowercase lastName))]
+    (assoc friend :id id)))
+
+(defn create-friend [db]
+  (fn [req]
+    (let [data (:body req)]
+      (db/create-friend db (-> data add-user gen-id))
+      (-> (response {:status "created"}) (status 201))
+      )))
+
 (defn handlers [db]
-  {:home (constantly (-> (response "hello world") (content-type "text/plain")))
-   :create-friend (constantly (-> (response "created") (content-type "text/plain") (status 201)))})
+  {:home          (constantly (-> (response "hello world") (content-type "text/plain")))
+   :create-friend (create-friend db)})
 
 (defn app
   "Takes a configuration map, a store object (i.e. to interact with the database),
      and a clock object (i.e. for timebased operations), and returns a ring request handler."
   [db]
   (-> (scenic-handler r/routes (handlers db))
-      ;wrap-json-response
+      wrap-json-response
       ;(m/wrap-exceptions c/error-handler)
       (wrap-defaults api-defaults)
-      ;(wrap-json-body {:keywords? true})
+      (wrap-json-body {:keywords? true})
       ;(m/wrap-allow-access-all)
       ))
 
--- test/dunbar_api/handler_test.clj
+++ test/dunbar_api/handler_test.clj
@@ -3,13 +3,33 @@
             [ring.mock.request :as mock]
             [dunbar-api.handler :as h]
             [dunbar-api.routes :as r]
-            ))
+            [cheshire.core :as json]
+            [dunbar-api.db :as db]))
 
-(def app (h/app nil))
+(def db (db/create-db))
+(db/rollback-db db nil)
+(db/migrate-db db nil)
+
+(def app (h/app db))
 
 (facts "handler returns something"
        (-> (mock/request :get "/") app :body) => "hello world")
 
-(facts "there is a post friends route"
-       (let [resp (-> (mock/request :post (r/path-for :create-friend)) app)]
-         (:status resp) => 201))
+(facts "can post and retrieve a friend"
+       (let [body (json/generate-string {:firstName "David" :lastName "Bowie"})
+             req (-> (mock/request :post (r/path-for :create-friend))
+                     (mock/body body)
+                     (mock/content-type "application/json; charset=utf-8"))]
+         (let [resp (-> req app)]
+           (:status resp) => 201
+           (future-fact "response has url to resource")
+           (future-fact "resource url returns data")
+           (db/retrieve-friend db "david-bowie") => {:firstName "David"
+                                                     :lastName  "Bowie"
+                                                     :user      "john"
+                                                     :id        "david-bowie"}
+           )
+         )
+       )
+
+(db/stop-db db)
