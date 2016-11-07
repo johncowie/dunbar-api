957f6302cb0facc97b3e9b30ef7b390874b9cb7c
John Cowie <john@imin.co>
2016-11-07 11:53:07 +0000

    Created table for user tokens

--- resources/migrations/001-initialise-tables.up.sql
+++ resources/migrations/001-initialise-tables.up.sql
@@ -1,7 +1,13 @@
-CREATE TABLE IF NOT EXISTS friends (
+CREATE TABLE friends (
     id varchar(100) NOT NULL,
     user_id varchar(100) NOT NULL,
     first_name varchar(50) NOT NULL,
     last_name varchar(50) NOT NULL,
     PRIMARY KEY(id, user_id)
 );
+
+CREATE TABLE user_tokens (
+    user_id varchar(100) NOT NULL,
+    token varchar(100) NOT NULL,
+    expiry TIMESTAMP WITH TIME ZONE NOT NULL
+)
--- test/dunbar_api/migrations_test.clj
+++ test/dunbar_api/migrations_test.clj
@@ -5,40 +5,45 @@
             [clojure.string :as str]
             [dunbar-api.test-utils :as u]))
 
-(defn adhoc-query-succeeds? [connection-pool query-str]
+(defn adhoc-query-succeeds? [db query-str]
   (try
-    (with-open [conn (.getConnection connection-pool)]
+    (with-open [conn (.getConnection (:pool db))]
       (boolean (jdbc/query {:connection conn} [query-str])))
     (catch Exception e
       (println (.getMessage e))
       false)))
 
-(defn column-exists? [connection-pool table-and-col-name]
+(defn column-exists? [db table-and-col-name]
   (let [[table-name column-name] (str/split table-and-col-name #"\.")
         query (format "SELECT %s FROM %s LIMIT 1;" column-name table-name)]
-    (when (adhoc-query-succeeds? connection-pool query)
+    (when (adhoc-query-succeeds? db query)
       table-and-col-name)))
 
-(defn table-exists? [connection-pool table-name]
+(defn table-exists? [db table-name]
   (let [query (format "SELECT * FROM %s LIMIT 1;" table-name)]
-    (when (adhoc-query-succeeds? connection-pool query)
+    (when (adhoc-query-succeeds? db query)
       table-name)))
 
 (u/with-db
   (fn [db]
     (facts "migration 001"
-           (let [pool (:pool db)]
-             (db/migrate-db db "001-initialise-tables")
-             (facts "creates friends table"
-                    (table-exists? pool "friends") => truthy
-                    (column-exists? pool "friends.user") => truthy
-                    (column-exists? pool "friends.id") => truthy
-                    (column-exists? pool "friends.first_name") => truthy
-                    (column-exists? pool "friends.last_name") => truthy
-                    (db/rollback-db db nil)
-                    (table-exists? pool "friends") => falsey
-                    (future-fact "can call rollback with a specific ID - non-inclusive")
-                    (future-fact "throws exception for non-existant migration id")
-                    ;(m/run-migrations db)
-                    (db/stop-db db)
-                    )))))
+           (db/migrate-db db "001-initialise-tables")
+           (facts "creates friends table and tokens table"
+                  (table-exists? db "friends") => truthy
+                  (column-exists? db "friends.user_id") => truthy
+                  (column-exists? db "friends.id") => truthy
+                  (column-exists? db "friends.first_name") => truthy
+                  (column-exists? db "friends.last_name") => truthy
+                  (table-exists? db "user_tokens") => truthy
+                  (column-exists? db "user_tokens.user_id") => truthy
+                  (column-exists? db "user_tokens.token") => truthy
+                  (column-exists? db "user_tokens.expiry") => truthy
+                  (db/rollback-db db nil)
+                  (table-exists? db "friends") => falsey
+                  (table-exists? db "user_tokens") => falsey
+                  (future-fact "can call rollback with a specific ID - non-inclusive")
+                  (future-fact "throws exception for non-existant migration id")
+                  ;(m/run-migrations db)
+                  (db/migrate-db db nil)
+                  (db/stop-db db)
+                  ))))
